bad3160856979e44f1f02ff1c12f8c3d9a32de20
Siddiq <siddiqladaf786@gmail.com>
2025-03-06 09:08:02 +0530

    Create ThreadExample.java

---
+++ ThreadExample.java
@@ -0,0 +1,23 @@
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - Count: " + i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread();
+        MyThread thread2 = new MyThread();
+
+        thread1.start(); // Start the first thread
+        thread2.start(); // Start the second thread
+    }
+}
